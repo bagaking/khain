54cb30432b7899fa566ca9e58e696b2072d1e1ef
bagaking <zfaustk@qq.com>
2018-09-25 01:12:23 +0800

    feat: using leveldb

---
+++ src/utils/db.js
@@ -0,0 +1,33 @@
+'using strict'
+
+/*
+    docs here : https://github.com/Level/levelup
+ */
+
+const levelup = require('levelup')
+const leveldown = require('leveldown')
+
+
+module.exports = class {
+    constructor(path) {
+        this.db = levelup(leveldown('./mydb'))
+    }
+
+    async get(key) {
+        await new Promise((rsv, rej) =>
+            this.db.get('name', (err, value) => {
+                if (err) rej(err); // likely the key was not found
+                else rsv(value);
+            })
+        );
+    }
+
+    async put(key, value) {
+        await new Promise((rsv, rej) =>
+            this.db.put('name', (err) => {
+                if (err) rej(err); // likely the key was not found
+                else rsv();
+            })
+        );
+    }
+}
